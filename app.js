d31185fc919a36562733821977bce5f49d38454f
Hasan1284 <hasansevindik1284@gmail.com>
2020-04-27 13:34:45 +0200

    date objesi çalısıldı

---
+++ app.js
@@ -0,0 +1,23 @@
+ // Date Objeckt
+
+ let d = new Date();
+ let birthday = new Date(1980,3,5);
+
+// Set metotları
+
+d.setFullYear(2019);
+d.setHours(15);
+d.setMinutes(46);
+
+
+
+
+ // Get metotları
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getFullYear());
+console.log(d.getHours());
+
+console.log(d.getFullYear() - birthday.getFullYear());
+ console.log(d);
+ console.log(typeof d);
