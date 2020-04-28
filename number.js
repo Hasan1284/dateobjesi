8a64d6829c4725f20db356b4c42543b207412aa4
Hasan1284 <hasansevindik1284@gmail.com>
2020-04-28 13:17:57 +0200

    number denemeleri yapıldı

---
+++ number.js
@@ -0,0 +1,34 @@
+// bugün Number objesi işlenecek
+
+let val;
+
+// val = Number("10");
+// val = parseInt("10"); stringi numaraya çevirir
+// val = parseInt("10.5"); tam sayı olarak döndürür
+// val = parseFloat("10.5"); numbera çevirir ve virgüllü halinide alır
+// val = parseInt("a10a");
+
+// val = isNaN("10"); girilen değerin numara olmadığını true false deger olarak girilir
+// val = isNaN("a10");
+
+// var num = 10.23232342;
+// val = num.toPrecision(5); parantez içine girilen rakam kadarını alıyorsun
+// val = num.toFixed(2); virgulden sonrasını paranteze giren rakam kadar fiksiliyor
+
+// val = Math.PI;
+// val = Math.round(2.4); enyakın sayıyay yuvarlar
+// val = Math.round(2.7);
+// val = Math.ceil(2.4); yukarıya yuvarlar
+// val = Math.floor(2.4); aşağıya yuvarlar
+// val = Math.sqrt(64); karekökü alır
+// val = Math.pow(2,3); a nın b ninci kuvvetini alır
+// val = Math.abs(-100); herzaman mutlak değer alır pozitif döndürür
+// val = Math.min(1,2,3,4,5); bu metodun min ve max ı verilen dizinin en küçügünü ve en büyüğünü döndürür
+// val = Math.random(); 0 ile 1 arasında random rakam veriyor *10 yapınca 10 katı rakamları random veriyor
+// val = Math.floor(Math.random()*10); bu yöntemle hem küsürattan kurtarıyoruz hem de 1 ile 10 arasında random yapabiliyoruz.
+// mesela yukarıdaki yöntemle elimizdeki resimleri random olarak çağırabiliriz
+
+
+
+console.log(val);
+console.log(typeof val);
